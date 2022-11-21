05fac24ea46a536629241473311175d1f174661a
carloslalinde <54185636+carloslalinde@users.noreply.github.com>
2022-11-20 22:31:12 -0600

    Add project files.

---
+++ ParallelTestingGrupo2/Base.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTestingGrupo2
+{
+    [TestFixture]
+    public class Base
+    {
+        public IWebDriver driver;
+
+    }
+
+}
---
+++ ParallelTestingGrupo2/Test.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ParallelTestingGrupo2
+{
+    [TestFixture]
+    [Parallelizable]
+    public class Test : Base
+    {
+        [Test]
+        public void PruebaLoginIncorrecto()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://localhost:44369/Login/Index");
+            driver.FindElement(By.Name("USERNAME")).SendKeys("Test");
+            driver.FindElement(By.Name("login")).Click();
+            System.Threading.Thread.Sleep(5000);
+
+        }
+
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test2 : Base
+        {
+            [Test]
+            public void PruebaCorrecto()
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://localhost:44369/Login/Index");
+                driver.FindElement(By.Name("USERNAME")).SendKeys("manu1");
+                driver.FindElement(By.Name("PASSWORD")).SendKeys("123");
+                driver.FindElement(By.Name("login")).Click();
+                System.Threading.Thread.Sleep(5000);
+
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test3 : Base
+        {
+            [Test]
+            public void PruebaOlvideContraseña()
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://localhost:44369/Login/Index");
+                driver.FindElement(By.Name("forgot")).Click();
+                driver.FindElement(By.Name("USERNAME")).SendKeys("test123");
+                driver.FindElement(By.Name("send")).Click();
+                System.Threading.Thread.Sleep(5000);
+
+            }
+        }
+
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test4 : Base
+        {
+            [Test]
+            public void PruebaNavegacion()
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://localhost:44369/Login/Index");
+                driver.FindElement(By.Name("USERNAME")).SendKeys("manu1");
+                driver.FindElement(By.Name("PASSWORD")).SendKeys("123");
+                driver.FindElement(By.Name("login")).Click();
+                driver.FindElement(By.Name("inventario")).Click();
+                System.Threading.Thread.Sleep(1000);
+                driver.FindElement(By.Name("ver inventario")).Click();
+                System.Threading.Thread.Sleep(5000);
+
+            }
+        }
+
+
+    }
+
+}
